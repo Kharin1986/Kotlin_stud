57200b5f03c5e6146831db4e6a25d82f0792d78d
Kharin1986 <harin_a_a@mail.ru>
2020-02-08 01:57:09 +0500

    Add button (by click on button viewState increments)

--- app/src/main/java/com/gb/kotlin_stud/MainActivity.kt
+++ app/src/main/java/com/gb/kotlin_stud/MainActivity.kt
@@ -2,11 +2,24 @@ package com.gb.kotlin_stud
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
+    lateinit var viewModel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
+        viewModel.viewState().observe(this, Observer { value ->
+            Toast.makeText(this, value, Toast.LENGTH_SHORT).show()
+        })
+        btn_push.setOnClickListener {
+            viewModel.buttonClick()
+        }
     }
 }
